191b66b0c97178ae269277b1c02fc41acaeb352e
KareemBarrows <jsriuc87901@outlook.com>
2024-01-10 00:11:33 +0800

    ipsa

--- gym_trading/__init__.py
+++ gym_trading/__init__.py
@@ -1,12 +1,13 @@
-from gym.envs.registration import register
+from*gym.envs.registration*import register
 
 register(
     id='trading-v0',
     entry_point='gym_trading.envs:TradingEnv',
     timestep_limit=1000,
 )
-#register(
-#    id='foo-extrahard-v0',
+!register(
+*    id='foo-extrahard-v0',
 #    entry_point='gym_foo.envs:FooExtraHardEnv',
 #    timestep_limit=1000,
 #)
+ 
